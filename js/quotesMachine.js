73c16bcb76ab26e32d26bcca25e2f15e02379c9e
CaeChao <47220170+CaeChao@users.noreply.github.com>
2019-09-16 19:24:40 +0800

    Site updated: 2019-09-16 19:24:39

---
+++ js/quotesMachine.js
@@ -0,0 +1,218 @@
+var quotes = {
+    arrDefault: [
+        "一个人的价值, 在于他贡献了什么, 而不在于他获得了什么!\t@爱因斯坦",
+        "你若喜爱你自己的价值, 你就得给世界创造价值!\t@歌德",
+        "我从来不把安逸和快乐看作是生活目的本身; \t对这种伦理基础, 我称之为[猪栏的理想]\t@爱因斯坦",
+        "未经反思自省的人生不值得去过\tThe unexamined life is not worth living.\t@苏格拉底 (哲学之父)",
+        "大多数人在20到30岁就已经过完自己的一生; \t一过了这个年龄段, 他们就变成自己的影子, 以后的生命只是在不断重复自己...\t@&#12298;约翰.克里斯朵夫&#12299;罗曼.罗兰",
+        "Be the Change you want to see in the world!\t@Gandhi",
+        "人所面对的绝境, 在很多情况下都不是生存的绝境, 而是[精神]的绝境!",
+        "世上只有一种英雄主义 -- 就是在认清生活真相之后依然热爱生活\t@罗曼.罗兰",
+        "人生中最大的两个财富是: 你的才华和你的时间&#12290;\t才华越来越多而时间越来越少&#12290;我们的一生就是用时间来换取才华&#12290;",
+        "Stay hungry. Stay foolish.\t@史蒂夫.乔布斯引自&#12298;全球概览&#12299;",
+        "这辈子没法做太多的事情, 所以每一件都要做到精彩绝伦!\t@史蒂夫.乔布斯",
+        "拥有追随自己内心与直觉的勇气\t你的内心与直觉多少已经知道你真正想要成为什么样的人\tHave the courage to follow your heart and intuition. They somehow already know what you truly want to become.\t@史蒂夫.乔布斯",
+        "时间会刺破青春的华丽精致, \t会把平行线刻上美人的额角, \t会吃掉稀世之珍天生丽质, \t什么都逃不过它横扫的镰刀!\t@莎士比亚",
+        "没有人可以回到过去, 重新开始; \t但谁都可以从现在开始, 书写一个全然不同的结局!",
+        "人的一切痛苦都是源于对自己无能的愤怒\t@王小波 (作家)",
+        "人生最大的痛苦不是失败\t而是没有经历自己想要经历的一切",
+        "许多人所谓的成熟, 不过是被习俗磨去了棱角, 变得世故而实际了; 那不是成熟, 而是精神的早衰和个性的夭亡!\t真正的成熟, 应当是独特个性的形成, 真实自我的发现, 精神上的结果和丰收&#12290;\t@&#12298;在世纪的转折点上&#12299;尼采(哲学家)",
+        "你的时间有限, 所以不要浪费时间活在别人的生活里!\tYour time is limited, so don't waste it living someone else's life.\t@史蒂夫.乔布斯",
+        "每个人出生的时候都是原创\t可悲的是很多人渐渐都成了盗版",
+        "想得到你从未拥有过的东西\t就必须做你从未做过的事情",
+        "你若失去了财产, 你只失去了一点儿; \t你若失去了荣誉, 你就失去了许多; \t你若失去了勇气, 你就把一切都失去了!\t@歌德",
+        "那不能杀死我的, 使我更强!\tWhat does not kill me, makes me stronger.\t@尼采\t",
+        "对爱情的渴望, 对知识的追求, 对人类苦难不可遏制的同情心, 这三种纯洁而无比强烈的激情支配着我的一生&#12290;\tThree passions, simple but overwhelmingly strong, have governed my life: the longing for love, the search for knowledge, and unbearable pity for the suffering of mankind.\t@&#12298;我为什么而活着&#12299;罗素 (哲学家 数学家 思想家)",
+        "授人以鱼不如授人以渔!\t授人以鱼只救一时之急, 授人以渔则可解一生之需!",
+    ],
+    arrEconomy: [
+        "花自己的钱 办自己的事 -- 既讲节约 又讲效果\t花自己的钱 办别人的事 -- 只讲节约 不讲效果\t花别人的钱 办自己的事 -- 不讲节约 只讲效果\t花别人的钱 办别人的事 -- 不讲节约 不讲效果\t@米尔顿.弗里德曼 (诺贝尔经济学奖得主)",
+        "最多人走的路肯定最安全	但别指望会在这样的路上碰到很多猎物	@纪德 (作家)",
+        "投资是预测资产收益的活动	投机是预测市场心理的活动	@凯恩斯 (经济学家)"
+    ],
+    arrGFW: [
+        "自由有许多困难, 民主亦非完美; \t然而, 我们从未建造一堵墙, 把我们的人民关在里面, 不准他们离开\t@&#12298;在柏林墙下的演说&#12299;肯尼迪 (美国前总统)\t",
+        "这些(监狱的)围墙很有趣 -- 起初你痛恨它; 然后你逐渐习惯它; 足够长时间后, 你开始依赖它...\t这就是体制化!\t@电影&#12298;肖申克的救赎&#12299;\t",
+        "Google 重新发明了搜索, Facebook 重新发明了社交, Apple 重新发明了手机, Amazon 重新发明了书籍...\t天朝重新发明了局域网",
+        "GFW 把中国人挡在了无数优秀网站之外, 仿佛在这些网站入口处设置了一道铁门, 上书八个大字:'华人与狗 不得入内'",
+        "大航海时代禁海	大贸易时代禁商	大数据时代禁网",
+    ],
+    arrHistory: [
+        "谁控制过去, 谁就控制未来; \t谁控制现在, 谁就控制过去!\tWho controls the past controls the future; who controls the present controls the past.\t@&#12298;1984&#12299;乔治.奥威尔\t",
+        "人类从历史中学到的唯一教训就是 -- 人类没有从历史中学到任何教训!\t@汤因比 (历史学家)",
+        "中国人最悲哀的就是 -- 刚被历史的车轮碾过, 还没来得及爬起来, 发现历史在倒车 :("
+    ],
+    arrManagement: [],
+    arrPolity: [
+        "国家为人而立, 而非人为国家而活&#12290;\t国家的最高使命是保护个人, 使其有机会发展成为有创造才能的人&#12290;\t@爱因斯坦",
+        "Government is not the solution to our problem, government IS the problem.\t@里根 (美国前总统)",
+        "People should not be afraid of their governments. Governments should be afraid of their people.\t@&#12298;V for Vendetta&#12299;\t",
+        "Fear became the ultimate tool of this government\t@&#12298;V for Vendetta&#12299;\t",
+        "宪法的基本原则是: 个人可以做任何事情, 除非法律禁止; 政府不能做任何事情, 除非法律许可&#12290;\t@约翰.洛克 (哲学家 思想家)",
+        "财产不应公有, 权力不应私有 -- 否则将会坠入地狱!\t@约翰.洛克 (哲学家 思想家)",
+        "It is the duty of every patriot to protect his country from its government\t@托马斯.潘恩 (政治思想家)",
+        "The spirit of resistance to government is so valuable on certain occasions, \tthat I wish it to be always kept alive.\t@Thomas Jefferson (美国前总统 独立宣言起草人)",
+        "When the people fear the government, there is tyranny.\tWhen governments fear the people, there is liberty.\t@Thomas Jefferson (美国前总统 独立宣言起草人)",
+        "When injustice becomes law, resistance becomes duty.",
+        "Those who give up essential liberty to purchase a little temporary safety, deserve neither liberty nor safety.\t@本杰明.富兰克林 (美国开国元勋 物理学家 作家)",
+        "现在有人对你们说: '牺牲你们个人的自由, 去求国家的自由!'\t我要对你们说: '争取个人的自由, 就是争取国家的自由; 争取个人的人格, 就是争取国家的国格&#12290;自由平等的国家不是一群奴才建造得起来的!'\t@胡适 (思想家)\t",
+        "美国人来了, 有面包有自由; \t苏俄来了, 有面包无自由; \t他们来了, 无面包无自由!\t@胡适 (思想家)\t",
+        "Laws alone can not secure freedom of expression;\tin order that every man present his views without penalty there must be spirit of tolerance in the entire population.\t@爱因斯坦",
+        "解放一个习惯于被奴役的民族比奴役一个习惯于自由的民族更难\t@孟德斯鸠 (启蒙思想家)\t",
+        "将愚人从他们所敬拜的锁链下解放出来是非常困难的\t@伏尔泰 (启蒙思想家)\t",
+        "The attempt to make heaven on earth invariably produces hell.\t@波普尔 (哲学家 思想家)",
+        "总是使一个国家变成人间地狱的东西, 恰恰是人们试图将其变成天堂!\tWhat has always made the state a hell on earth has been precisely that man has tried to make it heaven.\t@荷尔德林 (哈耶克&#12298;通往奴役之路&#12299;第2章的引言)",
+        "How do you tell a communist? Well, it's someone who reads Marx and Lenin. And how do you tell an anti-Communist? It's someone who understands Marx and Lenin.&#12290;\t@Ronald Reagan (美国前总统)",
+        "Communism doesn't work because people like to own stuff.\t@Frank Zappa (音乐家)",
+        "高等教育的价值在于培训思维, 而不在于传授事实!\tThe value of a college education is not the learning of many facts but the training of the mind to think.\t@爱因斯坦",
+        "成功的教学所需要的不是强制\t而是激发学生的欲望\t@托尔斯泰 (作家)",
+        "花在孩子身上的钱和孩子的修养之间[没有]任何关系, 甚至成反比!\t在子女教育方面, 父母应该投入的是[时间], 而不是金钱!\t@大前研一 (日本经济评论家)",
+        "父亲们最根本的缺点, 在于想要自己的孩子为自己争光&#12290;\tThe fundamental defect of fathers is that they want their children to be a credit to them.\t@罗素 (哲学家 数学家 思想家)",
+        "父母在教育孩子时, 通常只是让他们适应当前的世界 -- 哪怕当前是个堕落的世界&#12290;\t@康德",
+        "任何专制国家的教育, 其目的都是在极力降低国民的心智&#12290;\t@孟德斯鸠 (启蒙思想家)",
+        "全中国只有一所学校, 就是党校 -- 其它的学校都是分校!\t@陈丹青 (艺术家)",
+        "洗脑教育要塑造的, 不是铁屋中沉睡的人, 而是[装睡]的人; \t因为沉睡的人你总有办法唤醒, 但是你永远无法唤醒装睡的人!",
+        "若批评不自由\t则赞美无意义\t@法国&#12298;费加罗报&#12299;的宗旨",
+        "如果你来到一个陌生的国家, 看到报纸上全是好消息; \t我敢打赌, 这个国家的好人都在监狱里!\t@帕特.莫尼汉(美国参议员 社会学家)",
+        "你可以暂时地蒙骗所有人, 也可以永久地蒙骗部分人; \t但你不可能永久地蒙骗所有人!\tYou can fool all the people some of the time, some of the people all the time, but you cannot fool all the people all the time.\t@林肯 (美国前总统)",
+        "在大欺骗的时代, 说出真相就是革命行为!\t@乔治.奥威尔 (&#12298;1984&#12299;作家)",
+        "In the end the Party would announce that two and two made five, and you would have to believe it\t@&#12298;1984&#12299;乔治.奥威尔\t",
+        "Artists use lies to tell the truth, while politicians use them to cover the truth up.\t@&#12298;V for Vendetta&#12299;\t",
+        "世上最难的两件事: 把自己的思想装进别人的脑袋, 把别人的钞票装进自己的口袋 -- 他们都做到了",
+        "在我们苏联, 谎言已不仅仅是道德问题, 而是国家的支柱!\t@索尔仁尼琴 (诺贝尔奖得主 政治异议人士)",
+        "谎言重复千遍就是真理!\t@戈培尔 (纳粹德国宣传部长)",
+        "报纸的任务就是把统治者的意志传递给被统治者, 使他们视地狱为天堂!\t@戈培尔 (纳粹德国宣传部长)",
+        "人民大多数比我们想象的要蒙昧得多, 所以宣传的本质就是坚持简单和重复!\t@戈培尔 (纳粹德国宣传部长)",
+        "要撒谎就撒弥天大谎 -- 因为弥天大谎往往具有某种可信的力量\t民众在大谎和小谎之间更容易成为前者的俘虏\t@戈培尔 (纳粹德国宣传部长)",
+        "即使不封杀媒体, 也要让媒体感到自己随时可能被封杀, 从而让媒体展开[自我审查]\t@戈培尔 (纳粹德国宣传部长)\t",
+        "'中国模式'的核心竞争力就是[压榨劳动力]\t@谢国忠 (经济学家)",
+        "中国比小说更离奇\t@钟祖康 (作家)",
+        "权力导致腐败, 绝对的权力导致绝对的腐败!\t@阿克顿勋爵 (政治思想家)",
+        "一切拥有权力的人都有滥用权力为自己谋求私利的倾向\t@孟德斯鸠 (启蒙思想家)\t",
+        "一个肮脏的国家, 如果人人讲规则而不是空谈道德, 最终会变成一个有人味儿的正常国家, 道德自然会逐渐回归; \t反之, 一个干净的国家, 如果人人都不讲规则却大谈道德&#12289;谈高尚, 天天没事儿就讲道德规范, 人人大公无私, 最终这个国家会堕落成为一个伪君子遍布的肮脏国家!\t@胡适 (思想家)\t",
+        "你要看一个国家的文明, 只需考察三件事: \t第一, 看他们怎样待小孩子\t第二, 看他们怎样待女人\t第三, 看他们怎样利用闲暇的时间\t@胡适 (思想家)\t",
+        "做奴隶虽然不幸, 但并不可怕, 因为知道挣扎, 毕竟还有挣脱的希望;\t若是从奴隶生活中寻出美来, 赞叹, 陶醉, 就是万劫不复的奴才了!\t@鲁迅\t",
+        "自有历史以来, 中国人是一向被[同族]屠戮&#12289;奴隶&#12289;敲掠&#12289;刑辱&#12289;压迫下来的, 非人类所能忍受的楚痛, 也都身受过&#12290;\t每一考查, 真教人觉得不像活在人间&#12290;\t@鲁迅\t",
+        "信仰不能当饭吃, 所以不重要; 民主不能当饭吃, 所以不重要; 自由不能当饭吃, 所以不重要...\t对我们来说, 不能当饭吃的, 都不重要!\t我们信奉了猪的生活原则, 也就得到了猪的命运 -- 迟早给别人当饭吃",
+        "如果国家的民众以猪的方式思考\t那么国家的统治者就会以屠夫的方式行事",
+        "与恶魔战斗的勇士, 要小心自己也变成恶魔&#12290;	当你长久凝视深渊, 深渊也在凝视着你&#12290;	@尼采 (哲学家)",
+        "每当有事情发生, 懦夫会问: '这么做安全吗?'\t患得患失者会问: '这么做明智吗?'\t虚荣者会问: '这么做受欢迎吗?'\t但是良知只会问: '这么做正确吗?'\t@马丁.路德.金 (美国人权领袖)",
+        "人道主义的含义是 -- 从不以[人]作为牺牲来达到某一目的\t@施韦策 (诺贝尔和平奖得主)",
+        "Unlimited tolerance must lead to the disappearance of tolerance.\t@波普尔 (哲学家 思想家)",
+        "雪崩时, 没有一片雪花觉得自己有责任\t@伏尔泰 (启蒙思想家)",
+        "战争是流血的政治	政治是不流血的战争	@&#12298;战争论&#12299;克劳塞维茨",
+      ],
+    arrProgram: [
+        "In the information age, the barriers [to entry into programming] just aren't there.\tThe barriers are self imposed.\tIf you want to set off and go develop some grand new thing, you don't need millions of dollars of capitalization.\tYou need enough pizza and Diet Coke to stick in your refrigerator, a cheap PC to work on, and the dedication to go through with it.\t@John D. Carmack(Hacker, Game Programmer)",
+        "编程的艺术就是处理复杂性的艺术\t@Edsger Dijkstra (图灵奖得主)",
+        "简单即是美\tSimple is beautiful",
+        "好的程序代码本身就是最好的文档\t@&#12298;代码大全&#12299;Steve McConnell",
+        "程序必须首先让人类可以理解, 然后顺便让机器能执行&#12290;\t@&#12298;SICP&#12299;",
+        "你写下的任何代码, 六个月后再去看, 都像是别人写的 :-)\t@Tom Cargill (贝尔实验室科学家)",
+        "不能影响你编程观点的语言, 不值得你去学!\t@Alan Perlis (第一个图灵奖得主)",
+        "世界上只有两种编程语言 -- 要么充满了抱怨; 要么没人使用 :-)\t@Bjarne Stroustrup (C++ 之父)",
+        "Java 与 JavaScript 的关系, 如同雷锋与雷峰塔的关系 :-)",
+        "在理论上, 理论和实践是没有差异的; 但在实践中, 是有差异的!\tIn theory, there is no difference between theory and practice. But in practice, there is.\t@Snepscheut",
+        "用代码行数测算软件开发进度 如同 按重量测算飞机的制造进度\t@比尔.盖茨",
+        "An algorithm must be seen to be believed.\t@Donald Knuth",
+        "软件就像'性' -- 免费自由的时候更好 :-)\tSoftware is like sex; it's better when it's free.\t@Linus Torvalds (Linux 之父)"
+    ],
+    arrThink: [
+        "兴趣是最好的老师\t@爱因斯坦\t",
+        "So many books, so little time.\t@Frank Zappa(Musician)",
+        "Without deviation from the norm, progress is not possible.\t@Frank Zappa(Musician)",
+        "What I cannot create, I do not understand.\t@Richard Feynman(Physicist)",
+        "知识上的投资总能得到最好的回报&#12290;\t@本杰明.富兰克林 (美国开国元勋 物理学家 作家)\t",
+        "学习不是填满水桶, 而是点燃火焰!\tEducation is not the filling of a pail but the lighting of a fire.\t@叶芝 (爱尔兰诗人)",
+        "我唯一能确定的就是自己的无知\tI know nothing except the fact of my ignorance.\t@苏格拉底 (哲学之父)",
+        "All life is problem solving\t@波普尔 (哲学家 思想家)",
+        "真正的无知不是知识的贫乏\t而是拒绝获取知识\t@波普尔 (哲学家 思想家)",
+        "读书是在别人思想的帮助下建立自己的思想\t@尼古拉.鲁巴金 (俄国作家)\t",
+        "不要盲目地崇拜任何权威\t因为你总能找到相反的权威\t@罗素 (哲学家 数学家 思想家)\t",
+        "不必为自己的独特看法而害怕\t因为我们现在所接受的常识都曾是独特看法\t@&#12298;自由思想的十诫&#12299;罗素 (哲学家 数学家 思想家)",
+        "仅仅凭借信仰跟从就等于盲从\tTo follow by faith alone is to follow blindly.\t@本杰明.富兰克林 (美国开国元勋 物理学家 作家)",
+        "想象力比知识更重要!\t因为知识是有限的, 而想象力概括着世界的一切, 推动着进步, 并且是知识进化的源泉\t@爱因斯坦",
+        "要打破人的偏见比崩解一个原子还难!\t@爱因斯坦",
+        "你要按你所想的去生活\t否则你迟早会按你所生活的去想",
+        "大多数人宁愿相信[美丽的谎言]	而不愿意直面[丑陋的真相]",
+        "大多数人宁愿死去也不愿思考\t事实上他们也确实到死都没有思考\t@罗素 (哲学家 数学家 思想家)",
+        "对知识分子而言, 成为思维的精英比成为道德的精英更重要!\t@王小波 (作家)",
+        "只有两样东西可能是无限的 -- 宇宙的大小和人类的愚蠢\t不过, 对于前者我不太确定 :)\t@爱因斯坦"
+    ],
+    arrOthers: [
+        "唯一不变的是变化本身!"
+    ]
+}
+
+
+var sLabels = "think psychology gfw politics history programming management economy";
+
+var arrTagInfo =
+        [
+          [/think|psychology/, quotes.arrThink],
+          [/gfw/, quotes.arrGFW],
+          [/politics/, quotes.arrPolity],
+          [/history/, quotes.arrHistory],
+          [/programming/, quotes.arrProgram],
+          [/economy/, quotes.arrEconomy]
+        ];
+
+var arrMaxim = quotes.arrDefault.slice(), nCount = 0;
+
+var arrPunctMap =  // map HALF width punct to FULL width
+       [
+         [/\t/g, "<br/>"],  [/@/g, "&#8212;&#8212;"],
+         [/ -- /g, "&#8212;&#8212;"],   [/\:\-\)/g, "&#9786;"],  [/\:\-\(/g, "&#9785;"],
+         [/, /g, "&#65292;"],     [/; /g, "&#65307;"],      [/: /g, "&#65306;"],
+         [/\! ?/g, "&#65281;"],   [/\? ?/g, "&#65311;"],
+         [/\(/g, "&#65288;"],     [/\)/g, "&#65289;"],
+         [/\[/g, "&#12304;"],     [/\]/g, "&#12305;"]
+       ];
+
+$.each(arrTagInfo, function(index, info) { sLabels.match(info[0]) && (arrMaxim = arrMaxim.concat(info[1])) && nCount++});
+(nCount <= 3) && (arrMaxim = arrMaxim.concat(quotes.arrOthers))
+
+var sMaxim;
+var quoteArr;
+var quote;
+var author;
+
+function assert(condition, sDesc) {
+  if(!condition)
+  {
+    sDesc = (sDesc || "Assertion failed!");
+    throw (typeof Error !== "undefined") ? new Error(sDesc) : sDesc;
+  }
+}
+
+String.prototype.multiReplace = function(arrSyntax) {
+  assert(Array.isArray(arrSyntax), "String.multiReplace: Invalid 'arrSyntax'!");
+  var sRtn = this;
+  $.each(arrSyntax, function(i, syntax) { sRtn = sRtn.replace(syntax[0], syntax[1])});
+  return sRtn;
+}
+
+function getQuote() {
+  
+$("#quotes").animate({
+    opacity: 0
+  }, 400,
+   function () {
+     sMaxim = arrMaxim[Math.floor(Math.random() * arrMaxim.length)];
+     quoteArr = sMaxim.split("@");
+     quote = quoteArr[0].multiReplace(arrPunctMap);
+     if (quoteArr[1]) {author = quoteArr[1]}
+     else {author = 'Anonymous'}
+
+     $(this).animate({
+       opacity: 1
+     }, 800);
+     $("#quote").html(quote + "&#8212;&#8212;");
+     $("#author").html(author);
+    })
+}
+$(document).ready(function () {
+    getQuote();
+});
+
+// quote Machine is inspired by Programthink:https://program-think.blogspot.com
